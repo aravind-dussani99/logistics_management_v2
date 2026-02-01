e9aa49f7775805934a7b1255bcb289946a9fb8df
aravindareddy <aravind.dussani99@gmail.com>
2026-02-01 10:58:07 +0000

    feat: terraform config

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -1,12 +1,12 @@
 # Buckets
 locals {
   logitrack_replica_buckets = {
-    "logitrack-attachments-replica" = { public = false }
-    "logitrack-frontend-replica"    = { public = false }
+    "logitrack-attachments-0123-replica" = { public = false }
+    "logitrack-frontend-0123-replica"    = { public = false }
   }
 
   logitrack_buckets = {
-    "logitrack-attachments" = { public = false }
-    "logitrack-frontend"    = { public = true }
+    "logitrack-attachments-0123" = { public = true }
+    "logitrack-frontend-0123"    = { public = true }
   }
 }
